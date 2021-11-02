0471279e5f36322cb490828258642cfd57693d88
ukkiran <65678960+ukkiran@users.noreply.github.com>
2021-11-02 16:56:53 +0530

    Update java_Jenkinsfile.groovy

--- vars/java_Jenkinsfile.groovy
+++ vars/java_Jenkinsfile.groovy
@@ -10,33 +10,34 @@ def call(){
                     branch: Branch
                 ])
                 stage('Read GlobalConfig & Specs'){
-                try {
-                println "reading the specs from Specs repository"
-                def specsDir = "."
-                println "specs version" + specsDir
-                    if(fileExists(specsDir + "/specs.yaml")){
-                    ci_template = readYaml file : specsDir + "/specs.yaml"
-                    specs = specs + ci_template
-                    println "reading specs file" + specs
-                    }
+                    try {
+                    println "reading the specs from Specs repository"
+                    def specsDir = "."
+                    println "specs version" + specsDir
+                        if(fileExists(specsDir + "/specs.yaml")){
+                        ci_template = readYaml file : specsDir + "/specs.yaml"
+                        specs = specs + ci_template
+                        println "reading specs file" + specs
+                        }
 
-                    println "reading the global config from resources"
-                    def request = libraryResource "com/org/service/config.yaml"
-                    config = readYaml text: request
-                    println "reading config file" + config
+                        println "reading the global config from resources"
+                        def request = libraryResource "com/org/service/config.yaml"
+                        config = readYaml text: request
+                        println "reading config file" + config
+                    }
+                    catch(Exception e) {
+                        println "Error in reading specs file : " + e.getMessage()
+                        throw e
+                    }
                 }
-                     catch(Exception e) {
-                         println "Error in reading specs file : " + e.getMessage()
-                         throw e
-                     }
-                }
+            }
             stage('Build') {
                 println 'build stage'
             }
-            }
+        }
         catch (Exception e) {
             println 'ERROR in the pipeline: ' + e.getMessage()
             throw e
         }
-        }
     }
+}
